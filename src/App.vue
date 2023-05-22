629bdfda2ac9d10509cd31884d0d4e072dc54e5c
Jasmin-rios <jasminrp.jrp@gmail.com>
2023-05-23 00:19:58 +0200

    footer-solucionado

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@ import { RouterLink, RouterView } from 'vue-router';
 import header_bar from  './components/header_bar.vue';
 import searching_bar from './components/searching_bar.vue';
 import events from './components/events.vue';
-/* import categories from './components/categories.vue'; */
-/*import footer_bar from './components/footer_bar.vue'; */
+import footer_bar from './components/footer_bar.vue';
 
 export default{
   name: 'Home',
@@ -12,6 +11,7 @@ export default{
     header_bar,
     searching_bar,
     events,
+    footer_bar,
     }}
 
 </script>
@@ -19,13 +19,13 @@ export default{
 <template>
   <header_bar />
   <searching_bar/>
-  <!-- <categories /> -->
   <events />
-  <!--<footer_bar /> -->
+  <footer_bar />
 </template>
 
 <style>
   template {
     margin: 0px;
   }
+  
 </style>
--- src/components/footer_bar.vue
+++ src/components/footer_bar.vue
@@ -5,19 +5,83 @@
 </script>
 
 <template>
-    <h3>Your Account</h3>
-        <ul>
-            <li>Log In</li>
-            <li>Sign Up</li>
-            <li></li>
-        </ul>
-    <h3>Search</h3>
-        <ul>
-            <li>Log In</li>
-            <li>Sign Up</li>
-            <li></li>
-        </ul>
+    <section class="footer">
+        <div class="all-options">
+            <div class="footer-options">
+                <p>Your Account</p>
+                <ul>
+                    <li>Log In</li>
+                    <li>Sign Up</li>
+                </ul>
+            </div>
+            <div class="footer-options">
+                <p>Discover</p>
+                <ul>
+                    <li>Type of Events</li>
+                    <li>Recommendations</li>
+                    <li>More information</li>
+                </ul>
+            </div>
+            <div class="footer-options">
+                <p>LiviT</p>
+                <ul>
+                    <li>About Us</li>
+                    <li>Newletter</li>
+                </ul>
+            </div>
+        </div>
+        <!-- <div class="footer-social-media">
+            <div id="Follow-us">
+                <p>Follow us</p>
+                <div id="icons-box">
+                    <img src="../assets/facebook.svg"/>
+                    <img src="../assets/instagram.svg"/>
+                    <img src="../assets/linkedin.svg">
+                    <img src="../assets/twitter.svg">
+                </div>
+            </div>
+            <img id= "logo-footer" src="../assets/Logo.png"/>
+        </div> -->
+    </section>
 </template>
 
-<style>
+<style scoped>
+    .footer {
+        display: flex;
+        background-color: black;
+    }
+
+    .all-options {
+        display: flex;
+        gap: 50px;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin: 10px auto;
+        align-items: flex-start;
+        flex-direction: row;
+        width: 90%;
+        height: 100%;
+        margin: 0 auto;
+        box-sizing: border-box;
+        padding: 20px;
+        
+        
+    }
+    .footer-options {
+        color: white;
+        gap: 4px;
+    }
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        font-size: 16px;
+    }
+
+    p {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 20x;
+        line-height: 24px;
+    }
+
 </style>
